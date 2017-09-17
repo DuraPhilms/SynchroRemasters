e87a74de5bec29850a011dd8fc87b58295c9b891
LNJ <git@lnj.li>
2017-09-17 22:12:27 +0200

    download-videos: Don't show mkdir errors

--- download-videos.sh
+++ download-videos.sh
@@ -8,7 +8,8 @@ elif command -v busybox >/dev/null 2>&1; then
     export DOWNLOADER_BIN="busybox wget"
 fi
 
-mkdir videos ; cd videos
+mkdir videos 2>/dev/null
+cd videos
 
 ## Required for PvA 01 ##
 $DOWNLOADER_BIN "https://archive.org/download/DuraPhilmsRemasterSources/Duraphilms_-_Die_Ankuendigung_des_Todestodes-uw76l0VnoL0.mkv"
