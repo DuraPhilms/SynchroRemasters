eda9e9605430a9514eeabc18595da143425d2752
JBBgameich <jbbgameich@gmail.com>
2017-09-17 21:52:37 +0200

    Autodetect which downloader is installed

--- download-videos.axel.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-mkdir videos ; cd videos
-
-## Required for PvA 01 ##
-axel https://archive.org/download/DuraPhilmsRemasterSources/Duraphilms_-_Die_Ankuendigung_des_Todestodes-uw76l0VnoL0.mkv
-
---
+++ download-videos.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+if command -v axel >/dev/null 2>&1; then
+    export DOWNLOADER_BIN="axel"
+elif command -v wget >/dev/null 2>&1; then
+    export DOWNLOADER_BIN="wget"
+elif command -v busybox >/dev/null 2>&1; then
+    export DOWNLOADER_BIN="busybox wget"
+fi
+
+mkdir videos ; cd videos
+
+## Required for PvA 01 ##
+$DOWNLOADER_BIN "https://archive.org/download/DuraPhilmsRemasterSources/Duraphilms_-_Die_Ankuendigung_des_Todestodes-uw76l0VnoL0.mkv"
