ad0be23061f47aed3b7a6567ec150b2ded569005
LNJ <git@lnj.li>
2017-09-17 21:25:13 +0200

    Add download script for video files
    This will add "DuraPhilms - Ankuendigung des Todestodes" required for
    PvA 01.
    There are two scripts one using axel (for more speed using multiple
    connections) and one using normal wget.

---
+++ download-videos.axel.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+mkdir videos ; cd videos
+
+## Required for PvA 01 ##
+axel https://archive.org/download/DuraPhilmsRemasterSources/Duraphilms_-_Die_Ankuendigung_des_Todestodes-uw76l0VnoL0.mkv
+
