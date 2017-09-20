d8cf33602ead1c4d4895a3022cb7cdb4e927f4c2
LNJ <git@lnj.li>
2017-09-20 19:15:05 +0200

    Video-download: Add new links

--- download-videos.sh
+++ download-videos.sh
@@ -11,9 +11,24 @@ fi
 mkdir videos 2>/dev/null
 cd videos
 
-## Required for PvA 01 ##
+## Gelistet in benoetigter Reihenfolge ##
+#$DOWNLOADER_BIN https://archive.org/download/DuraPhilmsRemasterSources/HP3.25fps.mkv
+$DOWNLOADER_BIN https://archive.org/download/DuraPhilmsRemasterSources/HPudPvA_01_Remake.mp4
 $DOWNLOADER_BIN https://archive.org/download/DuraPhilmsRemasterSources/Duraphilms_-_Die_Ankuendigung_des_Todestodes-uw76l0VnoL0.mkv
-
-## Required for PvA 02-14 ##
 $DOWNLOADER_BIN https://archive.org/download/DuraPhilmsRemasterSources/HPudPvA_Intro_1080Rmk.mp4
-
+$DOWNLOADER_BIN https://archive.org/download/DuraPhilmsRemasterSources/HPudPvA_02_Remake.mp4
+$DOWNLOADER_BIN https://archive.org/download/DuraPhilmsRemasterSources/HPudPvA_03_Remake.mp4
+#$DOWNLOADER_BIN https://archive.org/download/DuraPhilmsRemasterSources/HP2.25fps.mkv
+#$DOWNLOADER_BIN https://archive.org/download/DuraPhilmsRemasterSources/HP4.25fps.mkv
+#$DOWNLOADER_BIN https://archive.org/download/DuraPhilmsRemasterSources/HP5.25fps.mkv
+#$DOWNLOADER_BIN https://archive.org/download/DuraPhilmsRemasterSources/HPudPvA_04.mp4
+#$DOWNLOADER_BIN https://archive.org/download/DuraPhilmsRemasterSources/HPudPvA_05.mp4
+#$DOWNLOADER_BIN https://archive.org/download/DuraPhilmsRemasterSources/HPudPvA_06.mp4
+#$DOWNLOADER_BIN https://archive.org/download/DuraPhilmsRemasterSources/HPudPvA_07.mp4
+#$DOWNLOADER_BIN https://archive.org/download/DuraPhilmsRemasterSources/HPudPvA_08.mp4
+#$DOWNLOADER_BIN https://archive.org/download/DuraPhilmsRemasterSources/HPudPvA_09.mp4
+#$DOWNLOADER_BIN https://archive.org/download/DuraPhilmsRemasterSources/HPudPvA_10.mp4
+#$DOWNLOADER_BIN https://archive.org/download/DuraPhilmsRemasterSources/HPudPvA_11.mp4
+#$DOWNLOADER_BIN https://archive.org/download/DuraPhilmsRemasterSources/HPudPvA_12.mp4
+#$DOWNLOADER_BIN https://archive.org/download/DuraPhilmsRemasterSources/HPudPvA_13.mp4
+#$DOWNLOADER_BIN https://archive.org/download/DuraPhilmsRemasterSources/HPudPvA_14.mp4
